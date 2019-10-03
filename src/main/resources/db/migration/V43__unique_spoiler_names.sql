c814e6a8a3b04120a47ca968f2fe6b2cd1756a8d
Nathan Westlake <coraythan@gmail.com>
2019-10-03 12:30:58 -0700

    fix

--- src/main/resources/db/migration/V43__unique_spoiler_names.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE
-    IF EXISTS spoiler
-    ADD
-        CONSTRAINT spoiler_name_uk UNIQUE (card_title);
-
-ALTER TABLE
-    IF EXISTS spoiler
-    ADD
-        CONSTRAINT card_number_uk UNIQUE (card_number);
