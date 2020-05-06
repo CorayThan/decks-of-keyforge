6bff103111d69d0b41318f6fe0ddad620e3b21d3
Nathan Westlake <coraythan@gmail.com>
2020-05-05 22:39:40 -0700

    test publishing new version, and QA version

--- src/main/kotlin/coraythan/keyswap/config/Env.kt
+++ src/main/kotlin/coraythan/keyswap/config/Env.kt
@@ -2,6 +2,6 @@ package coraythan.keyswap.config
 
 enum class Env(val baseUrl: String) {
     dev("http://localhost:3000"),
-    qa("http://nathanwestlake.com"),
+    qa("https://decks-of-keyforge-qa-2.us-west-2.elasticbeanstalk.com"),
     prod("https://decksofkeyforge.com")
 }
