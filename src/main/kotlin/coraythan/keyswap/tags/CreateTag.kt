53bcc1f09105c98e4d36dabf435f7ec9fdf8f1c6
Nathan Westlake <coraythan@gmail.com>
2021-06-01 19:46:24 -0700

    maybe finally bulk listing good

--- src/main/kotlin/coraythan/keyswap/tags/CreateTag.kt
+++ src/main/kotlin/coraythan/keyswap/tags/CreateTag.kt
@@ -1,10 +1,13 @@
 package coraythan.keyswap.tags
 
 import coraythan.keyswap.generatets.GenerateTs
+import coraythan.keyswap.generatets.TsOptional
 
 @GenerateTs
 data class CreateTag(
         val name: String,
         val public: PublicityType,
+
+        @TsOptional
         val archived: Boolean = false,
 )
